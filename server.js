f7834210c7a9b943e8684b5ee97ab72d0dad37e0
i-python-com <wyp4wj@gmail.com>
2019-10-04 11:21:01 -0700

    Update scripts and serve static file in production

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -27,6 +27,18 @@ const usersRouter = require('./routes/users')
 
 app.use('/exercises', exercisesRouter)
 app.use('/users', usersRouter)
+
+// Serve the static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
