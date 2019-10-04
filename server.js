b448681e05d59f8088b7bfaae43b6cbc330f14d4
i-python-com <wyp4wj@gmail.com>
2019-10-04 11:21:01 -0700

    Update scripts and serve static file in production

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.ATLAS_URI
+const uri =
+  'mongodb+srv://admin:mean123@cluster0-yxvzh.gcp.mongodb.net/mern_exercise_tracker?retryWrites=true&w=majority'
 
 mongoose.connect(uri, {
   // connect local mongodb without username and password
@@ -27,6 +28,18 @@ const usersRouter = require('./routes/users')
 
 app.use('/exercises', exercisesRouter)
 app.use('/users', usersRouter)
+
+// Serve the static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
