dd3f62aabcc434a9ac2b20b80fa351e167cf7020
i-python-com <wyp4wj@gmail.com>
2019-10-05 01:04:25 -0700

    Update axios route

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -32,9 +32,7 @@ export default class CreateUser extends Component {
 
     console.log(user)
 
-    axios
-      .post('http://localhost:5000/users/add', user)
-      .then(res => console.log(res.data))
+    axios.post('/users/add', user).then(res => console.log(res.data))
 
     this.setState({
       username: ''
